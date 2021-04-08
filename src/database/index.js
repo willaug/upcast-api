42e5efdf0e620a29a4f52c2f9f4c771f1caeb246
William Augusto <william.santos315@outlook.com>
2021-04-08 15:02:09 -0300

    models connection

--- src/database/index.js
+++ src/database/index.js
@@ -3,16 +3,24 @@ const DB = require('../config/database')
 const connection = new Sequelize(DB)
 
 const User = require('../models/User')
+const Playlist = require('../models/Playlist')
+const Episode = require('../models/Episode')
 const Category = require('../models/Category')
 const ResetPassword = require('../models/ResetPassword')
 const Show = require('../models/Show')
 
 User.init(connection)
+Show.init(connection)
+Episode.init(connection)
+Playlist.init(connection)
 Category.init(connection)
 ResetPassword.init(connection)
-Show.init(connection)
 
-ResetPassword.associate(connection.models)
+User.associate(connection.models)
 Show.associate(connection.models)
+Episode.associate(connection.models)
+Playlist.associate(connection.models)
+Category.associate(connection.models)
+ResetPassword.associate(connection.models)
 
 module.exports = connection
