3860561fbd16703a05010528996e1f3011dfd083
William Augusto <william.santos315@outlook.com>
2021-04-12 17:57:48 -0300

    authenticate route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const rest = require('./rest')
 const search = require('./search')
+const authenticate = require('./authenticate')
 
 router.use(rest)
 router.use(search)
+router.use(authenticate)
 
 module.exports = router
