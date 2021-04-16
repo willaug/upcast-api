7b348921977fa2e6d72deab47cae0730eb7cd59c
William Augusto <william.santos315@outlook.com>
2021-04-16 17:02:40 -0300

    Account informations

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -1,6 +1,21 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
 class AccountController {
   async index (req, res) {
-    res.send('Olá, você está logado!')
+    const { authorization } = req.headers
+    const token = authorization && authorization.split(' ')[1]
+
+    const decoded = jwt.decode(token)
+
+    try {
+      const userFound = await User.findByPk(decoded.uid,
+        { attributes: ['uid', 'username', 'url_photo', 'email', 'createdAt', 'updatedAt'] })
+
+      return res.status(200).json(userFound)
+    } catch {
+      return res.status(500).json('Desculpe, mas algum erro ocorreu. Que tal tentar novamente?')
+    }
   }
 }
 
