121f5c7911b5423ffe7d19574d85cccc02cf2d67
William Augusto <william.santos315@outlook.com>
2021-04-16 16:50:05 -0300

    Auth middleware

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -3,6 +3,23 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const secret = process.env.ACCESS_TOKEN
 
-module.exports = async function (req, res, next) {
+module.exports = function (req, res, next) {
+  const { authorization } = req.headers
+  const token = authorization && authorization.split(' ')[1]
 
+  if (token === undefined || token === null) {
+    return res.status(401).json('É necessário estar autenticado para continuar.')
+  }
+
+  jwt.verify(token, secret, (err, response) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(403).json('Sua autorização expirou, autentique-se novamente.')
+      }
+
+      return res.status(403).json('Sua autorização é inválida, autentique-se.')
+    }
+
+    next()
+  })
 }
